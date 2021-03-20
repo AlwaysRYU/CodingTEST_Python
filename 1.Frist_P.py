71fb12833cac17fc380dc12f72f20f3daa9b40a6
alwaysRYU13 <aliverise@naver.com>
2021-03-20 19:59:10 +0900

    2021.03.20. 코테대비

---
+++ 1.Frist_P.py
@@ -0,0 +1,11 @@
+print('hello World')
+print('*' * 16)
+print('hello python!!')
+print('*' * 18)
+print('hello pycharm!!!!')
+
+print('hello python2222')
+
+print('git test')
+
+
--- Frist_P.py
+++
@@ -1,9 +0,0 @@
-print('hello World')
-print('*' * 16)
-print('hello python!!')
-print('*' * 18)
-print('hello pycharm!!!!')
-
-print('hello python2222')
-
-
