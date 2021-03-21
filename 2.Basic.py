8c953b0603203586200117b5b59a0dafa5ba66f5
alwaysRYU13 <aliverise@naver.com>
2021-03-21 16:36:13 +0900

    2021.03.21. 기본 함수들 기억하기

--- 2.Basic.py
+++ 2.Basic.py
@@ -7,4 +7,16 @@ print(c)
 이건 주석
 
 '''
+# 이것도 한줄 주석
+def sum(a,b) :
+    #더하기 함수
+    print(a+b)
+
+# split( seq = '', max split = -1) 문자열을 나누는 함수
+# seq : 문자열을 구분하는거
+# maxsplit : 분리할 문자개수를 지정할 때 사용됨
+examplemal = '우리는 모두 괜찮습니다.'
+print(examplemal)
+examplemal.split(' ')
+print(examplemal)
 
