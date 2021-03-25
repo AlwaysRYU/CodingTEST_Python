77b732171719bec981026efd4d16843e13205b5e
alwaysRYU13 <aliverise@naver.com>
2021-03-25 17:32:45 +0900

    2021.03.25. 프로그래머스 문제 풀이

--- 7.실전문제_두개뽑아서더하기.py
+++ 7.실전문제_두개뽑아서더하기.py
@@ -6,15 +6,21 @@ https://programmers.co.kr/learn/courses/30/lessons/68644?language=python3
 '''
 #내가 푼것
 numbers = [ 2, 1, 3, 4, 1]
+
 def solution(numbers):
     answer = []
     a = len(numbers)
+    
     for i in range(a) :
         temp1 = numbers[i]
+
+        #마지막일 경우는 탈출
         if i == a-1 : break 
+
         for j in range(i+1,a):
             temp2 = numbers[j]
             temp3 = temp1 + temp2
+            #값이 없을 경우 answer에 추가한다.
             if temp3 not in answer :
                 answer.append(temp3)
     
@@ -24,9 +30,13 @@ def solution(numbers):
 #3 더 간단한 풀이
 def solution2(numbers):
     answer = []
+
     for i in range(len(numbers)):
         for j in range(i+1, len(numbers)):
             answer.append(numbers[i] + numbers[j])
     return sorted(list(set(answer)))
+
+#리스트중복제거 메소드 set를 사용함으로써 더욱 간단하게 풀이할 수 있다.
+
 
 print(solution(numbers))
