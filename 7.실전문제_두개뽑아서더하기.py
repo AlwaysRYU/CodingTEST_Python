99940ef466db622171116d92e2153cd81787bb46
AlwaysRYU <aliverise@naver.com>
2021-03-25 17:35:55 +0900

    2021.03.25. 커밋이 안됨

--- 7.실전문제_두개뽑아서더하기.py
+++ 7.실전문제_두개뽑아서더하기.py
@@ -4,7 +4,7 @@ https://programmers.co.kr/learn/courses/30/lessons/68644?language=python3
 난이도 : Level 1
 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
 '''
-#내가 푼것
+#내가 푼것 커밋이 왜 안돼
 numbers = [ 2, 1, 3, 4, 1]
 
 def solution(numbers):
