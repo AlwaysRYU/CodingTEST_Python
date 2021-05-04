07e84fdec28966297f73fef05fccdf5413948d74
aliverise@naver.com <aliverise@naver.com>
2021-05-04 14:09:18 +0900

    2021.05.04. 튜플 최종

---
+++ 2021.5월/04.튜플.py
@@ -0,0 +1,43 @@
+# 튜플
+# https://programmers.co.kr/learn/courses/30/lessons/64065?language=python3
+
+
+s1, s2, s3, s4, s5 = "{{2},{2,1},{2,1,3},{2,1,3,4}}", "{{1,2,3},{2,1},{1,2,4,3},{2}}", "{{20,111},{111}}", "{{123}}" , "{{4,2,3},{3},{2,3,4,1},{2,3}}"
+
+def solution(s):
+    print(s)
+    arr = s[2:-2].split("},{")
+    print(arr)
+    # 지금은 str
+    arr2 = []
+    dic = {}
+
+    for i in range(len(arr)):
+        arr2.append(arr[i].split(','))
+        dic[len(arr2[i])] = arr[i].split(',')
+    print(arr2)
+    print(dic)
+
+    answer = []
+    for i in range(1, len(arr2)+1):
+        templist = dic[i]
+        for j in range(len(templist)):
+            if not int(templist[j]) in answer :
+                answer.append(int(templist[j]))
+    return answer
+
+print(solution(s1))
+print(solution(s2))
+print(solution(s3))
+# print(solution(s4))
+# print(solution(s5))
+
+# 다른 사람의 풀이
+def solution2(s):
+    # Counter : 길이 수를 세기
+    s = Counter(re.findall('\d+', s))
+    # a.items() : key value 쌍 을 얻는다.
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
+
+import re
+from collections import Counter
--- Python학습정리/딕셔너리.py
+++ Python학습정리/딕셔너리.py
@@ -9,6 +9,7 @@
 '''
 
 mydictionary = {}
+# 마우스 팽귄은 키, 3, 5는 값
 mydictionary = {"mouse" : 3, "penguin" : 5 }
 dic = { 'name' : 'maya pay' , 'phone': '010545' }
 a = { 1: [1,2,3], 2 : [4,5,6]}
@@ -21,5 +22,29 @@ print(mydictionary["mouse"])
 #이러면 3이 출력된다.
 
 # 1. 딕셔너리에 쌍 추가
+# a[키] = 값
 a[3] = [7,8,9]
 print(a)
+
+# 2. 딕셔너리 요소 삭제
+# del a[키]
+del dic['name']
+
+# 3. 딕셔너리 관련 함수
+# 3-1. items()
+# key value 쌍을 튜플로 묶은 값을 dict_items 객체로 돌려준다.
+# 리스트로 사용할 수 있다.
+print(a.items())
+templist = a.items()
+print(templist)
+
+# 3-2. keys()
+# 딕셔너리의 키만 모아서 객체를 돌려준다.
+print(list(a.keys()))
+
+# 3-3. values()
+# 마찬가지로 value만 모아서 돌려준다.
+
+# 3-4. clear()
+# 딕셔너리 안의 모든 요소를 삭제한다.
+
