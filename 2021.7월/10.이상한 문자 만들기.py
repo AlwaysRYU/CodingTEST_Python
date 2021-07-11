efcd068ce1a972c94e58584c29da4098e9bc7f21
aliverise@naver.com <aliverise@naver.com>
2021-07-11 15:46:58 +0900

    2021.07.11. 이상한 문자 만들기 해결

--- 2021.7월/10.이상한 문자 만들기.py
+++ 2021.7월/10.이상한 문자 만들기.py
@@ -4,4 +4,18 @@
 
 def solution(s):
     answer = ''
-    return answer
+    s = s.split(" ")
+    print(s)
+    for i in s :
+        for j in range(len(i)) :
+            if (j + 1) % 2 == 1 :
+                answer += i[j].upper()
+            else :
+                answer += i[j].lower()
+        answer += " "
+    return answer[:-1]
+print(solution("try hello world"))
+
+# 다른사람의 풀이
+def toWeirdCase(s):
+    return " ".join(map(lambda x: "".join([a.lower() if i % 2 else a.upper() for i, a in enumerate(x)]), s.split(" ")))
