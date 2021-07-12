c520f34fa48d0e0d48b4bc8b472df543f68cff79
aliverise@naver.com <aliverise@naver.com>
2021-07-12 22:07:39 +0900

    2021.07.12. 정수제곱 해결

---
+++ 2021.7월/12.정수제곱근판별.py
@@ -0,0 +1,23 @@
+# 정수 제곱근 판별
+# https://programmers.co.kr/learn/courses/30/lessons/12934
+
+# 나의 풀이
+def solution(n):
+    temp = 1
+    while(True) :
+        Number = temp * temp 
+        if Number == n :
+            return (temp + 1) * (temp + 1)
+        elif Number > n :
+            return -1
+        else :
+            temp += 1
+
+
+# 다른사람의 풀이
+def nextSqure(n):
+    sqrt = n ** (1/2)
+
+    if sqrt % 1 == 0:
+        return (sqrt + 1) ** 2
+    return 'no'
