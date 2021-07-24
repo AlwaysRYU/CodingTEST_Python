3e075b1b10fb80259a6b28996a76f195c96f3d21
aliverise@naver.com <aliverise@naver.com>
2021-07-24 17:57:26 +0900

    2021.07.24. 나머지 해결

--- 2021.7월/18.Q10자물쇠와 열쇠.py
+++ 2021.7월/18.Q10자물쇠와 열쇠.py
@@ -6,6 +6,33 @@
 # 달리 방법이 없는 것같다.
 # 전부 다해보는것 빼고
 
+def rotate(array):
+    N = len(array)
+    rotateArr = [[0] * N for _ in range(N) ]
+    # 행렬 생성하기
+    print(rotateArr)
+    for i in range(N) : 
+        for j in range(N) : 
+            rotateArr[i][j] = array[N-1-j][i]
+    return rotateArr
+
+def check(array):
+    # lock 돌면서 됐는가 확인하기.
+
+
+    return False
+
+# print(rotate([[0, 0, 0], [1, 0, 0], [0, 1, 1]]))
+print(rotate([[0, 0, 0, 1], [1, 0, 0, 1], [0, 1, 1, 0],[0, 1, 1, 0]]))
+
+
 def solution(key, lock):
-    answer = True
+    answer = False
+    # N * N 배열 back  만들기
+    # N 은 lock + 2(key -1)이다.
+
+    # 그리고 for문돌면서 
+    # lock 만큼 반복하기
+    # 4번돌리고 체크하면서 맞으면 true출력한다.   
+
     return answer
--- 2021.7월/19.Q11뱀.py
+++ 2021.7월/19.Q11뱀.py
@@ -1,9 +1,20 @@
-# TODO 
-# 19 18 16 
-# 자바 문제 2개
-# read me update
-# ssafy과제
-# 오늘 과제
-
 # 뱀
 # https://www.acmicpc.net/problem/3190
+
+
+N = int(input())
+board = [[False] * N for _ in range(N) ]
+
+apples = int(input())
+for i in range(apples):
+    y = input().split(" ")
+    # 사과 위치
+    board[y[0] -1][ y[1]-1 ] = True
+ 
+snake = [0,0]
+print(board)
+
+
+
+
+
